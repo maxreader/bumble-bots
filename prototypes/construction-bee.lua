f49d40f09e28e977046e327e92b8b5cf621281a5
60635199 <60635199+maxreader@users.noreply.github.com>
2020-05-20 02:01:17 -0500

    removed sprites, switched to tint version

--- prototypes/construction-bee.lua
+++ prototypes/construction-bee.lua
@@ -1,134 +1,180 @@
-local constructionBee = data.raw["construction-robot"]["construction-robot"]
-local constructionBeeCorpse = data.raw["corpse"]["construction-robot-remnants"]
 local buzzSounds = require("__bumble-bots__/prototypes/buzz-sounds")
 
-local icon_data = {
-    icon = "__bumble-bots__/graphics/icons/construction-bee.png",
-    icon_size = 64,
-    icon_mipmaps = 1
-}
-
-for k, v in pairs(icon_data) do
-    constructionBee[k] = v
-    data.raw["item"]["construction-robot"][k] = v
-    constructionBeeCorpse[k] = v
-end
-
-
--- Entity Sprites
-local sprites = 
-{
-  filename = "__bumble-bots__/graphics/entity/construction-bee/construction-bee.png",
-  priority = "high",
-  line_length = 16,
-  width = 66,
-  height = 76,
-  frame_count = 1,
-  shift = util.by_pixel(0,-4.5),
-  direction_count = 16,
-  scale = 0.5
-}
-
-constructionBee.idle = sprites
-constructionBee.in_motion = sprites
-constructionBee.in_motion.y = 76
-
--- Shadows
-local shadows = 
-{
-  filename = "__bumble-bots__/graphics/entity/construction-bee/construction-bee-shadow.png",
-  priority = "high",
-  line_length = 16,
-  width = 104,
-  height = 49,
-  frame_count = 1,
-  shift = util.by_pixel(33.5, 18.75),
-  direction_count = 16,
-  scale = 0.5,
-  draw_as_shadow = true
-}
-
-constructionBee.shadow_in_motion = shadows
-constructionBee.shadow_idle = shadows
-constructionBee.shadow_working = shadows
-constructionBee.shadow_in_motion.y = 49
-
--- Working Animation
-constructionBee.working =
-{
-  filename = "__bumble-bots__/graphics/entity/construction-bee/construction-bee-working.png",
-  priority = "high",
-  line_length = 2,
-  width = 66,
-  height = 76,
-  frame_count = 2,
-  shift = util.by_pixel(-0.25, -5),
-  direction_count = 16,
-  animation_speed = 0.3,
-  scale = 0.5
-}
-constructionBee.shadow_working = 
-{
-  filename = "__bumble-bots__/graphics/entity/construction-bee/construction-bee-shadow.png",
-  priority = "high",
-  line_length = 16,
-  width = 104,
-  height = 49,
-  frame_count = 1,
-  repeat_count = 2,
-  shift = util.by_pixel(33.5, 18.75),
-  direction_count = 16,
-  scale = 0.5,
-  draw_as_shadow = true
-}
-
-for _,spark in pairs(data.raw["construction-robot"]["construction-robot"].sparks) do
-    spark.tint = {r=232,g=172,b=19}
-end
-
--- Sounds
-constructionBee.working_sound = buzzSounds.low_buzz
-
--- Corpses
-constructionBeeCorpse.animation = make_rotated_animation_variations_from_sheet (4,
-{
-  filename = "__bumble-bots__/graphics/entity/construction-bee/construction-bee-corpse.png",
-  line_length = 1,
-  width = 120,
-  height = 114,
-  frame_count = 1,
-  variation_count = 1,
-  axially_symmetrical = false,
-  direction_count = 1,
-  shift = util.by_pixel(1, 1),
-  scale = 0.333
-})
-
-
-constructionBeeCorpse.ground_patch = 
-{
-    sheet =
+local function set_construction_sprites_with_mask(robot, tint)
+    local constructionBee = data.raw["construction-robot"][robot]
+    
+    -- Entity Sprites
+    local sprites = {
+        layers = {
+        {
+            filename = "__bumble-bots__/graphics/entity/construction-bee/construction-bee.png",
+            priority = "high",
+            line_length = 16,
+            width = 66,
+            height = 76,
+            frame_count = 1,
+            shift = util.by_pixel(0,-4.5),
+            direction_count = 16,
+            scale = 0.5
+        },{
+            filename = "__bumble-bots__/graphics/entity/construction-bee/construction-bee-mask.png",
+            priority = "high",
+            line_length = 16,
+            width = 66,
+            height = 76,
+            frame_count = 1,
+            shift = util.by_pixel(0,-4.5),
+            direction_count = 16,
+            scale = 0.5,
+            flags = {"mask"},
+            tint = tint,
+        },
+    }}
+    
+    constructionBee.idle = sprites
+    constructionBee.in_motion = sprites
+    constructionBee.in_motion.y = 76
+    
+    -- Shadows
+    local shadows = 
     {
-        filename = "__base__/graphics/entity/biter/hr-blood-puddle-var-main.png",
-        flags = { "low-object" },
-        line_length = 4,
-        variation_count = 4,
-        frame_count = 1,
-        width = 164,
-        height = 134,
-        shift = {0, 0.125},
-        tint = {r = 0.2, g = 0.8, b = 0.1, a = 1},
-        scale = 0.2
+      filename = "__bumble-bots__/graphics/entity/construction-bee/construction-bee-shadow.png",
+      priority = "high",
+      line_length = 16,
+      width = 104,
+      height = 49,
+      frame_count = 1,
+      shift = util.by_pixel(33.5, 18.75),
+      direction_count = 16,
+      scale = 0.5,
+      draw_as_shadow = true
     }
-}
-
---Make bees show up on map
-if settings.startup["bumble-bots-show-bots-on-map"].value then
-    flags = constructionBee.flags
-    for k,v in pairs(flags) do
-        if v == "not-on-map" then
-            table.remove(flags,k)
-        end
+    
+    constructionBee.shadow_in_motion = shadows
+    constructionBee.shadow_idle = shadows
+    constructionBee.shadow_working = shadows
+    constructionBee.shadow_in_motion.y = 49
+    constructionBee.shadow_working.repeat_count = 2
+    
+    -- Working Animation
+    constructionBee.working = {
+        layers = {
+        {
+            filename = "__bumble-bots__/graphics/entity/construction-bee/construction-bee-working.png",
+            priority = "high",
+            line_length = 2,
+            width = 66,
+            height = 76,
+            frame_count = 2,
+            shift = util.by_pixel(-0.25, -5),
+            direction_count = 16,
+            animation_speed = 0.3,
+            scale = 0.5
+        },{
+            filename = "__bumble-bots__/graphics/entity/construction-bee/construction-bee-working-mask.png",
+            priority = "high",
+            line_length = 2,
+            width = 66,
+            height = 76,
+            frame_count = 2,
+            shift = util.by_pixel(-0.25, -5),
+            direction_count = 16,
+            animation_speed = 0.3,
+            scale = 0.5,
+            flags = {"mask"},
+            tint = tint,
+        },
+    }}
+    
+    for _,spark in pairs(constructionBee.sparks) do
+        spark.tint = {r=232,g=172,b=19}
     end
-    constructionBee.map_color = {r = 255, g = 213, b = 25, a = 153}
+    
+    -- Sounds
+    constructionBee.working_sound = buzzSounds.low_buzz
+    
+    -- Corpses
+    local constructionBeeCorpse = data.raw["corpse"][robot.."-remnants"]
+    if data.raw["corpse"][robot.."-remnants"] == nil then
+        constructionBeeCorpse = table.deepcopy(data.raw["corpse"]["construction-robot-remnants"])
+        constructionBeeCorpse.name = robot.."-remnants"
+        data:extend{constructionBeeCorpse}
+    end
+
+    constructionBeeCorpse.animation = make_rotated_animation_variations_from_sheet(4, {
+        layers = {
+            {
+                filename = "__bumble-bots__/graphics/entity/construction-bee/construction-bee-corpse.png",
+                line_length = 1,
+                width = 120,
+                height = 114,
+                frame_count = 1,
+                variation_count = 1,
+                axially_symmetrical = false,
+                direction_count = 1,
+                shift = util.by_pixel(1, 1),
+                scale = 0.333
+            },{
+                filename = "__bumble-bots__/graphics/entity/construction-bee/construction-bee-corpse-mask.png",
+                line_length = 1,
+                width = 120,
+                height = 114,
+                frame_count = 1,
+                variation_count = 1,
+                axially_symmetrical = false,
+                direction_count = 1,
+                shift = util.by_pixel(1, 1),
+                scale = 0.333,
+                flags = {"mask"},
+                tint = tint,
+
+            }
+        }     
+    })
+    
+    
+    constructionBeeCorpse.ground_patch = 
+    {
+        sheet =
+        {
+            filename = "__base__/graphics/entity/biter/hr-blood-puddle-var-main.png",
+            flags = { "low-object" },
+            line_length = 4,
+            variation_count = 4,
+            frame_count = 1,
+            width = 164,
+            height = 134,
+            shift = {0, 0.125},
+            tint = {r = 0.2, g = 0.8, b = 0.1, a = 1},
+            scale = 0.2
+        }
+    }
+    
+    local icons = {
+        {
+            icon = "__bumble-bots__/graphics/icons/construction-bee.png",
+            icon_size = 64,
+        },{
+            icon = "__bumble-bots__/graphics/icons/construction-bee-mask.png",
+            icon_size = 64,
+            tint = tint
+        },
+    }
+    
+    constructionBee.icons = icons
+    data.raw["item"][robot].icons = icons
+    constructionBeeCorpse.icons = icons
+    
+    --Make bees show up on map
+    if settings.startup["bumble-bots-show-bots-on-map"].value then
+        flags = constructionBee.flags
+        for k,v in pairs(flags) do
+            if v == "not-on-map" then
+                table.remove(flags,k)
+            end
+        end
+        constructionBee.map_color = {r = 255, g = 213, b = 25, a = 153}
+    end
 end
+
+set_construction_sprites_with_mask("construction-robot", {255,200,80,155})
--- prototypes/logistic-bee.lua
+++ prototypes/logistic-bee.lua
@@ -1,9 +1,10 @@
 -- Changes logistic robots into Logistic Bees
 ---------------------------------------------------------------------------------------------------
 local buzzSounds = require("__bumble-bots__/prototypes/buzz-sounds")
+local mods = mods
 
-local function set_logistic_sprites(level, color)
-    local logisticBee = data.raw["logistic-robot"][level]
+local function set_logistic_sprites(robot, color)
+    local logisticBee = data.raw["logistic-robot"][robot]
     local sprites = {
         filename = "__bumble-bots__/graphics/entity/logistic-bee/logistic-bee-"..color.."/logistic-bee-"..color..".png",
         priority = "high",
@@ -49,10 +50,10 @@ local function set_logistic_sprites(level, color)
 
 
     -- Corpses
-    local logisticBeeCorpse = data.raw["corpse"][level.."-remnants"]
-    if data.raw["corpse"][level.."-remnants"] == nil then
+    local logisticBeeCorpse = data.raw["corpse"][robot.."-remnants"]
+    if data.raw["corpse"][robot.."-remnants"] == nil then
         logisticBeeCorpse = table.deepcopy(data.raw["corpse"]["logistic-robot-remnants"])
-        logisticBeeCorpse.name = level.."-remnants"
+        logisticBeeCorpse.name = robot.."-remnants"
         data:extend{logisticBeeCorpse}
     end
 
@@ -91,7 +92,7 @@ local function set_logistic_sprites(level, color)
     
     for k, v in pairs(icon_data) do
         logisticBee[k] = v
-        data.raw["item"][level][k] = v
+        data.raw["item"][robot][k] = v
         logisticBeeCorpse[k] = v
     end
     
@@ -114,7 +115,155 @@ local function set_logistic_sprites(level, color)
 
 end
 
-set_logistic_sprites("logistic-robot", "plain")
+local function set_logistic_sprites_with_mask(robot, tint)
+    local logisticBee = data.raw["logistic-robot"][robot]
+    local sprites = {
+        layers = {
+        {
+            filename = "__bumble-bots__/graphics/entity/logistic-bee/logistic-bee-old.png",
+            priority = "high",
+            line_length = 16,
+            width = 80,
+            height = 84,
+            frame_count = 1,
+            shift = util.by_pixel(0, -3),
+            direction_count = 16,
+            scale = 0.5,
+        },{
+            filename = "__bumble-bots__/graphics/entity/logistic-bee/logistic-bee-mask.png",
+            priority = "high",
+            line_length = 16,
+            width = 80,
+            height = 84,
+            frame_count = 1,
+            shift = util.by_pixel(0, -3),
+            direction_count = 16,
+            scale = 0.5,
+            flags = {"mask"},
+            tint = tint,
+        },
+    }}
+
+    local shadows = {
+        filename = "__bumble-bots__/graphics/entity/logistic-bee/logistic-bee-shadow.png",
+        priority = "high",
+        line_length = 16,
+        width = 115,
+        height = 57,
+        frame_count = 1,
+        shift = util.by_pixel(31.75, 19.75),
+        direction_count = 16,
+        y = 57,
+        scale = 0.5,
+        draw_as_shadow = true
+    }
+
+    logisticBee.idle = sprites
+    logisticBee.idle_with_cargo = sprites
+    logisticBee.in_motion = sprites
+    logisticBee.in_motion_with_cargo = sprites
+
+    logisticBee.idle.y = sprites.layers[1].height
+    logisticBee.in_motion_with_cargo.y = sprites.layers[1].height * 2
+    logisticBee.in_motion.y = sprites.layers[1].height * 3
+
+    logisticBee.shadow_idle = shadows
+    logisticBee.shadow_idle_with_cargo = shadows
+    logisticBee.shadow_in_motion = shadows
+    logisticBee.shadow_in_motion_with_cargo = shadows
+
+    logisticBee.shadow_idle.y = shadows.height
+    logisticBee.shadow_in_motion_with_cargo.y = shadows.height * 2
+    logisticBee.shadow_in_motion.y = shadows.height * 3
+
+    -- Corpses
+    local logisticBeeCorpse = data.raw["corpse"][robot.."-remnants"]
+    if data.raw["corpse"][robot.."-remnants"] == nil then
+        logisticBeeCorpse = table.deepcopy(data.raw["corpse"]["logistic-robot-remnants"])
+        logisticBeeCorpse.name = robot.."-remnants"
+        data:extend{logisticBeeCorpse}
+    end
+
+    logisticBeeCorpse.animation = make_rotated_animation_variations_from_sheet(4, {
+        layers = {
+            {
+                filename = "__bumble-bots__/graphics/entity/logistic-bee/logistic-bee-corpse.png",
+                line_length = 1,
+                width = 116,
+                height = 114,
+                frame_count = 1,
+                variation_count = 1,
+                axially_symmetrical = false,
+                direction_count = 1,
+                shift = util.by_pixel(1, 1),
+                scale = 0.4
+            },{
+                filename = "__bumble-bots__/graphics/entity/logistic-bee/logistic-bee-corpse-mask.png",
+                line_length = 1,
+                width = 116,
+                height = 114,
+                frame_count = 1,
+                variation_count = 1,
+                axially_symmetrical = false,
+                direction_count = 1,
+                shift = util.by_pixel(1, 1),
+                scale = 0.4,
+                flags = {"mask"},
+                tint = tint,
+
+            }
+        }     
+    })
+    logisticBeeCorpse.ground_patch = {
+        sheet = {
+            filename = "__base__/graphics/entity/biter/hr-blood-puddle-var-main.png",
+            flags = {"low-object"},
+            line_length = 4,
+            variation_count = 4,
+            frame_count = 1,
+            width = 164,
+            height = 134,
+            shift = {0, 0},
+            tint = {r = 0.2, g = 0.8, b = 0.1, a = 0.6},
+            scale = 0.2
+        }
+    }
+    
+    local icons = {
+            {
+                icon = "__bumble-bots__/graphics/icons/logistic-bee-icon.png",
+                icon_size = 64,
+            },{
+                icon = "__bumble-bots__/graphics/icons/logistic-bee-icon-mask.png",
+                icon_size = 64,
+                tint = tint
+            },
+    }
+    
+    logisticBee.icons = icons
+    data.raw["item"][robot].icons = icons
+    logisticBeeCorpse.icons = icons
+    
+    -- Sounds
+    logisticBee.working_sound = buzzSounds.medium_buzz
+
+    -- Changing cargo center
+    logisticBee.cargo_centered = {0.0, -0.75}
+
+    --Make bees show up on map
+    if settings.startup["bumble-bots-show-bots-on-map"].value then
+        local flags = logisticBee.flags
+        for k,v in pairs(flags) do
+            if v == "not-on-map" then
+                table.remove(flags,k)
+            end
+        end
+        logisticBee.map_color = {r=1,g=1,b=0.2,a=0.6}
+    end
+
+end
+
+set_logistic_sprites_with_mask("logistic-robot", {r = 1, g = 0.72, b = 0, a = 0.85})
 
 
 
@@ -122,16 +271,26 @@ set_logistic_sprites("logistic-robot", "plain")
 
 if mods["boblogistics"] then
     local mapping = {
-        ["yellow"] = "logistic-robot",
-        ["red"] = "bob-logistic-robot-2",
-        ["blue"] = "bob-logistic-robot-3",
-        ["purple"] = "bob-logistic-robot-4",
-        ["green"] = "bob-logistic-robot-5"
+        ["logistic-robot"] = {1, 0.8, 0.06, 0.9},
+        ["bob-logistic-robot-2"] = {1, 0.06, 0.06, 0.9},
+        ["bob-logistic-robot-3"] = {0.06, 0.2, 1, 0.9},
+        ["bob-logistic-robot-4"] = {0.545, 0.06, 1, 0.9},
+        ["bob-logistic-robot-5"] = {0.06, 1, 0.14, 0.9},
     }
-    for color, robot in pairs(mapping) do
-        functions.set_logistic_sprites(robot, color)
+    for robot, color in pairs(mapping) do
+        set_logistic_sprites_with_mask(robot, color)
     end
 end
 
+
+
+
+if mods["pyindustry"] then
+    set_logistic_sprites("py-logistic-robot-01", "red")
+end
+
+if mods["pyhightech"] then
+    set_logistic_sprites("logistic-robot-ht", "aqua")
+end
 
 
